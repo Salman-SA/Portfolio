7caee3a821a7cbe624b9e5d3cb6c59e959f8b443
Salman-SA <Salman777x7@gmail.com>
2025-12-24 01:08:17 +0300

    Remove resume
    Removed the download link for the resume and its associated confirmation dialog.

--- src/components/Vilian/Vilian.jsx
+++ src/components/Vilian/Vilian.jsx
@@ -25,18 +25,7 @@ const Vilian = () => {
             <p>passionate about web development and databases. I love building interactive web apps, designing clean UI, and managing data with SQL using DML and DDL.
                 Always exploring new tech to level up my skills and turn ideas into functional solutions.</p>
                 </div>
-            <div className="vilian-action" data-aos="fade-up" data-aos-delay="500">
-                <a
-                    href={SalmanAlyahya_CV}
-                    download="SalmanAlyahya_CV.pdf"
-                    className="vilian-resume"
-                    onClick={(e) => {
-                        const confirmDownload = window.confirm("Are you sure you want to download the resume?");
-                        if (!confirmDownload) {
-                            e.preventDefault();
-                        }
-                    }}
-                >
+            
 
                     <img src={pdf} alt='PDF Icon' className="resume-icon" />
                     <span>My Resume</span>
